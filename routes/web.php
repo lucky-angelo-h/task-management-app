2b5108b05fec3ab3b168b476efb8c98096023fe4
Lucky Angelo <driftgtr91@gmail.com>
2024-09-22 02:34:10 +0800

    Update web.php
    fix routing on login

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,14 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    // return view('welcome');
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Route::get('/members', [TaskController::class, 'create'])->name('members.index');
+    Route::get('/', [TaskController::class, 'dashboard'])->name('dashboard');
     Route::get('/dashboard', [TaskController::class, 'dashboard'])->name('dashboard');
 
     Route::get('/tasks', [TaskController::class, 'list'])->name('tasks.index');
